7f9b81c0568cd605eeb08b0641a038823a762b79
Sanyaur <gs.ujvari@gmail.com>
2022-04-28 19:07:23 +0200

    added more style

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,15 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
+const UserCard = ({ user: { id, avatar, email, first_name, last_name } }) => {
+  return (
+    <>
+      <Card key={id}>
+        <div>
+          <Avatar src={avatar} alt={first_name} />
+        </div>
+        <div>✉ {email}</div>
+        <Name>
+          {first_name} {last_name}
+        </Name>
+      </Card>
+    </>
+  );
+};
+
 const Card = styled.div`
+  width: 230px;
+  margin: 4rem 0.3rem !important;
   padding: 0.5rem;
   text-align: center;
   border: solid white 1px;
-  margin: 0.3rem;
   border-radius: 0.7rem;
   color: rgb(54, 54, 54);
   background: white;
-  flex-basis: 20%;
+
   & div {
     margin: 0.5rem 0;
   }
@@ -17,22 +34,14 @@ const Card = styled.div`
 
 const Avatar = styled.img`
   border-radius: 50%;
+  width: 128px;
+  height: 128px;
+  margin-top: -5rem;
 `;
 
-const UserCard = ({ user: { id, avatar, email, first_name, last_name } }) => {
-  return (
-    <>
-      <Card style={{ marginBottom: "1rem" }} key={id}>
-        <div>
-          <Avatar src={avatar} alt={first_name} />
-        </div>
-        <div>{email}</div>
-        <div>
-          {first_name} {last_name}
-        </div>
-      </Card>
-    </>
-  );
-};
+const Name = styled.div`
+  font-size: 1.5rem;
+  font-weight: bolder;
+`;
 
 export default UserCard;
